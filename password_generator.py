e304ac1fb0716d06c04db55715344822667f0437
JonEast87 <j.d.east@icloud.com>
2023-07-05 12:33:58 -0700

    creating dynamic list that will include digits or special characters depending on input for the random password

--- password_generator.py
+++ password_generator.py
@@ -8,7 +8,17 @@ def generate_password(min_length, numbers=True, special_characters=True):
     digits = string.digits
     special = string.punctuation
 
-    print(letters, digits, special)
+    characters = letters
+    # Next four lines checks if the parameters are true, if they are then they will be added to the letters list
+    if numbers:
+        characters += digits
+    if special_characters:
+        characters += special
+
+    pwd = ""
+    meets_criteria = False
+    has_number = False
+    has_special = False
 
 
 generate_password(10)
